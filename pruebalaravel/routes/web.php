c158313fb51a9aee2859542e19da95d79fd154cc
Lolis00 <lolis_380@hotmail.com>
2024-09-27 19:11:47 -0600

    26/09/2024avance

--- pruebalaravel/routes/web.php
+++ pruebalaravel/routes/web.php
@@ -2,6 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
+/* Route::get('/', function () {
     return view('welcome');
-});
+}); */
+
+Route::view('/', 'inicio')->name('rutainicio');
+
+Route::view('/formulario', 'formulario')->name('rutacacas');
+
+Route::view('/consultar', 'clientes')->name('rutaconsulta');
