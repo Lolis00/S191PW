23375d1c273dbac98e90e4539d0ed7ca6d31fa0e
Lolis00 <lolis_380@hotmail.com>
2024-10-18 19:10:54 -0600

    paso4
    hacer una portada de inicio

---
+++ practicaRepaso/resources/views/inicio.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Portada</title>
+    @vite(['resources/js/app.js'])
+
+    <style>
+        body, html {
+            height: 100%;
+        }
+        .full-height {
+            height: 100vh;
+        }
+    </style>
+    </head>
+<body>
+
+    <div class="d-flex flex-column justify-content-center align-items-center full-height">
+        <h1>Bienvenido a mi proyecto de repaso</h1>
+        <p>Nombre: Lolis</p>
+        <p>Proyecto: practicaRepasos</p>
+
+        <a href="{{ route('repaso1') }}" class="btn btn-primary">REPASO 1</a>
+        
+
+    </div>
+</body>
+</html>
--- practicaRepaso/routes/web.php
+++ practicaRepaso/routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
-Route::get('/', function () {
+use App\Http\Controllers\inicioController;
+use App\Http\Controllers\RepasoController;
+/*Route::get('/', function () {
     return view('welcome');
-});
+});*/
+
+Route::get('/', [InicioController::class, 'inicio'])->name('inicio');
+Route::get('/repaso1', [RepasoController::class, 'index'])->name('repaso1');
+Route::post('/convertir', [RepasoController::class, 'convertir'])->name('convertir');
+
+
