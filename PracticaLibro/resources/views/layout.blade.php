4c569d51970ebda4c57a8f2d02d4153e3af357e2
Lolis00 <lolis_380@hotmail.com>
2024-10-31 18:03:38 -0600

    Layout_Inicio_1

---
+++ PracticaLibro/resources/views/layout.blade.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>@yield('title')</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+
+    <style>
+        html, body {
+            height: 100%;
+            margin: 0;
+            display: flex;
+            flex-direction: column;
+        }
+        .container {
+            flex: 1;
+        }
+        footer {
+            background-color: #000;
+            color: #fff;
+            padding: 10px 0;
+            text-align: center;
+        }
+    </style>
+</head>
+<body>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        
+        <a class="navbar-brand" href="{{ route('inicio') }}">Biblioteca</a>
+        <div class="collapse navbar-collapse">
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('registro.libro') }}">Registro de Libro</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+
+    <div class="container mt-5">
+        @yield('content')
+    </div>
+
+    <footer class="text-center mt-5">
+    <p>&copy; Biblioteca - {{ date('d F Y') }}</p>
+</footer>
+</body>
+</html>
